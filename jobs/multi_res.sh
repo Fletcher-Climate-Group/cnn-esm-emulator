904e2396ad4a3dd767dd6b3b519ab45924ac9cbb
wmcnally <wmcnally@uwaterloo.ca>
2022-01-31 12:07:37 -0500

    update mist job

--- jobs/multi_res.sh
+++ jobs/multi_res.sh
@@ -7,7 +7,9 @@
 #SBATCH -p compute_full_node
 
 module load anaconda3
-conda activate tf2.2
+source activate py3.7-tf2.4-torch1.7
+source deactivate py3.7-tf2.4-torch1.7
+source activate py3.7-tf2.4-torch1.7
 python tf/train_multi_res.py \
   --data-dir $SCRATCH/data \
   --exp-dir $SCRATCH/experiments \
@@ -15,4 +17,5 @@ python tf/train_multi_res.py \
   --n-gpus 4 \
   --n-trials 40 \
   --n-test 20 \
-  --n-hr 0 1 2 3 4 5 10 15 20 30 40 50 60 70 80
+  --n-hr 0 1 2 3 4 5 10 15 20 30 40 50 60 70 80 \
+  --save-models
